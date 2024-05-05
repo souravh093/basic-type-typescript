e98ef0da7b1b3530e5b5e06dd57dd6a11947c4d3
Sourave Halder <souravh093@gmail.com>
2024-05-05 11:20:05 +0600

    static in OOP class

--- module3/src/3.6.ts
+++ module3/src/3.6.ts
@@ -1,42 +1,40 @@
 {
-    // access getter and setter
-  
-    class BankAccount {
-      constructor(
-        readonly id: number,
-        public name: string,
-        protected _balance: number // private not accessible for the child class then we need to use the protected method  
-      ) {}
+  // access getter and setter
 
-      // setter
-      set deposit(amount: number) {
-        this._balance += amount
-      }
-  
+  class BankAccount {
+    constructor(
+      readonly id: number,
+      public name: string,
+      protected _balance: number // private not accessible for the child class then we need to use the protected method
+    ) {}
+
+    // setter
+    set deposit(amount: number) {
+      this._balance += amount;
+    }
+
     //   addDeposit(amount: number) {
     //       this._balance += amount
     //       return amount
     //   }
-  
+
     // getter
     get Balance() {
-        return this._balance
+      return this._balance;
     }
-    
+
     //   getBalance() {
     //       return this._balance
     //   }
-    }
-  
-  
-    const poorManAccount = new BankAccount(110, "Mr: Poor", 70);
-  
-    // const deposit = poorManAccount.addDeposit(100)  // call the function
-    // const balance = poorManAccount.getBalance();    // call the function 
-  
-    const myBalance = poorManAccount.Balance
-    const myDeposit = poorManAccount.deposit = 50
-    console.log(myDeposit)
-    console.log(myBalance)
   }
-  
+
+  const poorManAccount = new BankAccount(110, "Mr: Poor", 70);
+
+  // const deposit = poorManAccount.addDeposit(100)  // call the function
+  // const balance = poorManAccount.getBalance();    // call the function
+
+  const myBalance = poorManAccount.Balance;
+  const myDeposit = (poorManAccount.deposit = 50);
+  console.log(myDeposit);
+  console.log(myBalance);
+}
---
+++ module3/src/3.7.ts
@@ -0,0 +1,20 @@
+{
+  // static
+  class Counter {
+    static count: number = 0
+
+   static increment() {
+      return Counter.count += 1;
+    }
+
+   static decrement() {
+      return Counter.count -= 1;
+    }
+  }
+
+  const instance1 = new Counter();
+  console.log(Counter.increment());
+  const instance2 = new Counter();
+  console.log(Counter.increment()); 
+
+}
